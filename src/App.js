c0c2d938dbeb6ae57405bb28032b9057b23eac26
Astoriaaaa <aysapkota@gmail.com>
2024-01-28 02:27:57 -0500

    sean killed me

--- src/App.js
+++ src/App.js
@@ -76,55 +76,49 @@ export function ChatPage(props) {
 
   }
   return (
-    <div style={{height: '92vh', padding: 20, display: 'flex'}}>
-
-      <div style={{display: 'flex',flex: 1, backgroundColor: '#131a54', borderRadius: 15, margin: 5, alignItems: 'center', flexDirection: 'column', color: 'grey'}}>
-        <div style={{marginTop: '2rem'}}>
-          <FaRegCircleUser color='grey' size={30} />
+    <div className='panel'>
+      <div className='subpanel-1'>
+        <div className='icon'>
+          <FaRegCircleUser color='white' size={30} />
         </div>
-        <div style={{marginTop: '2rem'}}>
-          <IoSparkles onClick={() => props.setChatPage(false)} color='grey' size={30} />
+        <div className='icon'>
+          <IoSparkles onClick={() => props.setChatPage(false)} color='white' size={30} />
         </div>
-        <div style={{marginTop: '2rem'}}>
-          <FiLogOut onClick={() => props.setUsername(undefined)} color='grey' size={30} />
+        <div className='icon'>
+          <FiLogOut onClick={() => props.setUsername(undefined)} color='white' size={30} />
         </div>
       </div>
 
-      <div style={{flex: 3, backgroundColor: 'white', borderRadius: 25, 
-                  margin: 5, display: 'flex', alignItems: 'center', 
-                  flexDirection: 'column', color: 'grey'}}>
-        <input style={{width: '13rem', marginTop: '2rem', backgroundColor: 'transparent', padding: 10, borderRadius: 10, borderStyle: 'solid'}} placeholder='search' type='text'/>
+      <div className='subpanel-2'>
+        <input className="input-1" placeholder='search' type='text'/>
         {chatPreview.map((data) => (
-        <div onClick={() => changeCurrentChat(data[0])} style={{backgroundColor: '#f2f2f2', width: '14rem', borderRadius: 10, padding: 5, height: '5rem', margin: '1rem', display: 'flex'}}>
+        <div style={{backgroundColor: '#f2f2f2', width: '100%', borderRadius: 10, padding: 5, height: '5rem', margin: '1rem', display: 'flex'}} onClick={() => changeCurrentChat(data[0])}>
           <div style={{flex: 3, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
-            <FaRegCircleUser flex={4} color='grey' size={40} />
-          </div>
-          <div style={{flex: 6, margin: 15}}>
-            <p style={{ margin: 0, fontSize: 12}}><strong>{data[0]}:</strong></p>
-            <p style={{ margin: 0, fontSize: 12}}>{data[1]}</p>
-          </div>
+              <FaRegCircleUser flex={4} color='grey' size={40} />
+            </div>
+            <div style={{flex: 6, margin: 15}}>
+              <p style={{ margin: 0, fontSize: 12}}><strong>{data[0]}:</strong></p>
+              <p style={{ margin: 0, fontSize: 12}}>{data[1]}</p>
+            </div>
         </div>
       ))}
       </div>
-
-      <div style={{height: '100%', display: 'flex', flexDirection: 'column', margin: 5}}>
-        <div style={{display: 'flex', flexDirection: 'flex-end', alignItems: 'center', backgroundColor: '#f2f2f2', borderTopLeftRadius: 25, borderTopRightRadius: 25, height: '5rem' }}>
-          <div style={{marginLeft: 15}}><FaRegCircleUser padding={10} size={30}/></div>
-          <p style={{marginLeft: '1rem'}}>{props.userName}</p>
+      <div className='subpanel-3'>
+        <div style={{display: 'flex', flexDirection: 'flex-end', alignItems: 'center', backgroundColor: '#f2f2f2', borderTopLeftRadius: 15, borderTopRightRadius: 15, height: '5rem' }}>
+            <div style={{marginLeft: 15}}><FaRegCircleUser padding={10} size={30}/></div>
+            <p style={{marginLeft: '1rem'}}>{props.userName}</p>
         </div>
-        <div style={{height: '100%', backgroundColor: '#dbdbdb', borderRadius: 25, display: 'flex', justifyContent: 'flex-end', flexDirection: 'column', borderTopLeftRadius: 0, borderTopRightRadius: 0}}>
-          <div style={{display: 'flex', flexDirection: 'column', padding: 10, overflowY: 'scroll'}}>
-            {currentChat.map((data) =>  (
-              <div style={{alignSelf: data[0] == props.userName? 'flex-end' : 'flex-start', backgroundColor: data[0] == props.userName? '#6899ed' : '#f2f2f2', borderRadius: 10, margin: '0.5rem'}}>
-                <p style={{margin: '0.5rem'}}>{data[1]}</p>
-              </div>
-            ))}
-          </div>
-          <div style={{display: 'flex' , justifyContent: 'center'}}>
-              <input onChange={e => currentMessage.current = e.target.value} style={{width: '55rem', backgroundColor: '#f2f2f2', padding: 10, borderRadius: 10, borderStyle: 'none', margin: '1rem'}} placeholder='message...'/>
-              <button onClick={() => UpdateMessages()} style={{color: 'white', width: '4rem', backgroundColor: '#6899ed', padding: 10, borderRadius: 10, borderStyle: 'none', margin: '1rem'}}>send</button>
-          </div>
-        </div>
+         <div className='message-panel'>
+          {currentChat.map((data) =>  (
+            <div style={{alignSelf: data[0] == props.userName? 'flex-end' : 'flex-start', backgroundColor: data[0] == props.userName? '#6899ed' : '#f2f2f2', borderRadius: 10, margin: '0.5rem'}}>
+              <p style={{margin: '0.5rem'}}>{data[1]}</p>
+            </div>
+          ))}
+         </div>
+         <div className='message-controls'>
+            <input onChange={e => currentMessage.current = e.target.value} placeholder='message...'/>
+            <button onClick={() => UpdateMessages()}>send</button>
+         </div>
       </div>
     </div>
   )
